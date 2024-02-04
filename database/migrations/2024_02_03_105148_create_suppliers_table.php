0c4c7ae3c09b8bb52af943ac9b222b8bfd18bf83
Arthur Novaes <arthurnovaes55@gmail.com>
2024-02-03 23:04:27 -0300

    feat: adc nullable p colunas

--- database/migrations/2024_02_03_105148_create_suppliers_table.php
+++ database/migrations/2024_02_03_105148_create_suppliers_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,13 +14,13 @@ return new class extends Migration
             $table->id();
             $table->string('cpf_cnpj')->unique();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('phone')->unique();
-            $table->string('address');
-            $table->string('number');
-            $table->string('city');
-            $table->string('state');
-            $table->string('country');
+            $table->string('email')->unique()->nullable();
+            $table->string('phone')->unique()->nullable();
+            $table->string('address')->nullable();
+            $table->string('number')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('country')->nullable();
             $table->timestamps();
         });
     }
