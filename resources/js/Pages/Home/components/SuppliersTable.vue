eab0ca53ccedbed047b53474ee88c6a875ff6b93
Arthur Novaes <arthurnovaes55@gmail.com>
2024-02-04 15:36:15 -0300

    refactor: extrai item do header para componente proprio

--- resources/js/Pages/Home/components/SuppliersTable.vue
+++ resources/js/Pages/Home/components/SuppliersTable.vue
@@ -3,56 +3,36 @@
     <template #head>
       <tr>
         <th scope="col" class="px-6 py-3">
-          <div class="flex items-center">
-            NOME FANTASIA
-            <button
-                @click="emit('getSuppliers',  {'orderBy': 'name', 'orderType': orderType == orderedHelper.Asc ? orderedHelper.Desc : orderedHelper.Asc })">
-              <svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                   viewBox="0 0 24 24">
-                <path
-                    d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
-              </svg>
-            </button>
-          </div>
+          <HeaderItem
+              label="NOME FANTASIA"
+              :order-type="orderType"
+              order-by="name"
+              @get-suppliers="emit('getSuppliers', $event)"
+          />
         </th>
         <th scope="col" class="px-6 py-3">
-          <div class="flex items-center">
-            CPF/CNPJ
-            <button
-                @click="emit('getSuppliers',  {'orderBy': 'cpf_cnpj', 'orderType': orderType == orderedHelper.Asc ? orderedHelper.Desc : orderedHelper.Asc })">
-              <svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                   viewBox="0 0 24 24">
-                <path
-                    d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
-              </svg>
-            </button>
-          </div>
+          <HeaderItem
+              label="CPF/CNPJ"
+              :order-type="orderType"
+              order-by="cpf_cnpj"
+              @get-suppliers="emit('getSuppliers', $event)"
+          />
         </th>
         <th scope="col" class="px-6 py-3">
-          <div class="flex items-center">
-            TELEFONE
-            <button
-                @click="emit('getSuppliers',  {'orderBy': 'phone', 'orderType': orderType == orderedHelper.Asc ? orderedHelper.Desc : orderedHelper.Asc })">
-              <svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                   viewBox="0 0 24 24">
-                <path
-                    d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
-              </svg>
-            </button>
-          </div>
+          <HeaderItem
+              label="TELEFONE"
+              :order-type="orderType"
+              order-by="phone"
+              @get-suppliers="emit('getSuppliers', $event)"
+          />
         </th>
         <th scope="col" class="px-6 py-3">
-          <div class="flex items-center">
-            EMAIL
-            <button
-                @click="emit('getSuppliers',  {'orderBy': 'email', 'orderType': orderType == orderedHelper.Asc ? orderedHelper.Desc : orderedHelper.Asc })">
-              <svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                   viewBox="0 0 24 24">
-                <path
-                    d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
-              </svg>
-            </button>
-          </div>
+          <HeaderItem
+              label="EMAIL"
+              :order-type="orderType"
+              order-by="email"
+              @get-suppliers="emit('getSuppliers', $event)"
+          />
         </th>
         <th scope="col" class="px-6 py-3">
           <span class="sr-only">Edit</span>
@@ -84,6 +64,7 @@
 <script setup lang="ts">
 import Table from "@/components/Table/Index.vue";
 import {PropType} from "vue";
+import HeaderItem from "@/Pages/Home/components/HeaderItem.vue";
 
 const orderedHelper = {
   Asc: 'asc',
