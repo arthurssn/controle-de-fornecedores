39be64cf8c61973d6909539a48153e5943b1d62e
Arthur Novaes <arthurnovaes55@gmail.com>
2024-02-04 09:22:01 -0300

    feat: add tailwind

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,11 +1,18 @@
 <template>
     <div>
-        <h1>
-            Hello, World!
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
         </h1>
+        <div>
+            Count is: {{ count }}
+            <button @click="count++">Click here to increment</button>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 
+import {ref} from "vue";
+
+const count = ref(0);
 </script>
