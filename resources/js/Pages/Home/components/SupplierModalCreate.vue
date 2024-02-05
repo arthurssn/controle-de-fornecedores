ac2484db8750fa60e4711fed2fea91214927467f
Arthur Novaes <arthurnovaes55@gmail.com>
2024-02-05 16:34:04 -0300

    feat: limpa form apos cadastrar

--- resources/js/Pages/Home/components/SupplierModalCreate.vue
+++ resources/js/Pages/Home/components/SupplierModalCreate.vue
@@ -1,59 +1,80 @@
 <script setup lang="ts">
 import Modal from "@/components/Modal/Modal.vue";
-import {reactive} from "vue";
-import axios from "axios";
-import Swal from 'sweetalert2'
+import { reactive } from "vue";
+import Swal from "sweetalert2";
 import FormSupplier from "@/Pages/Home/components/FormSupplier.vue";
 import SupplierService from "@/services/SupplierService";
 
 defineProps({
-  modalOpen: {
-    type: Boolean,
-    default: false
-  }
-})
+    modalOpen: {
+        type: Boolean,
+        default: false,
+    },
+});
 
-const emit = defineEmits(['update:modalOpen', 'submitSucess']);
+const emit = defineEmits(["update:modalOpen", "submitSucess"]);
 
 const form: ISupplier = reactive({
-  name: '',
-  cpf_cnpj: '',
-  email: '',
-  phone: '',
-  state: '',
-  city: '',
-  address: '',
-  number: '',
-  country: 'Brasil'
-})
+    name: "",
+    cpf_cnpj: "",
+    email: "",
+    phone: "",
+    state: "",
+    city: "",
+    address: "",
+    number: "",
+    country: "Brasil",
+});
 
 function createSupplier() {
-  SupplierService.create(form)
-      .then(() => {
-        emit('update:modalOpen', false);
-        Swal.fire('Sucesso', 'Fornecedor cadastrado com sucesso!', 'success');
-        emit('submitSucess');
-      })
-      .catch((error) => {
-        Swal.fire('Ocorreu um erro', error.response.data.message, 'error');
-      });
+    SupplierService.create(form)
+        .then(() => {
+            emit("update:modalOpen", false);
+            Swal.fire(
+                "Sucesso",
+                "Fornecedor cadastrado com sucesso!",
+                "success"
+            );
+            emit("submitSucess");
+            form.name =
+                form.cpf_cnpj =
+                form.email =
+                form.phone =
+                form.state =
+                form.city =
+                form.address =
+                form.number =
+                    "";
+        })
+        .catch((error) => {
+            Swal.fire("Ocorreu um erro", error.response.data.message, "error");
+        });
 }
-
 </script>
 
 <template>
-  <Modal :modal-open="modalOpen" @update:modalOpen="emit('update:modalOpen', $event)" title="Cadastrar Fornecedor">
-    <template #body>
-      <form id="form" class="flex flex-col gap-4" @submit.prevent="createSupplier">
-        <FormSupplier :form="form"/>
-      </form>
-    </template>
-    <template #footer>
-      <button type="submit"
-              form="form"
-              class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        Cadastrar
-      </button>
-    </template>
-  </Modal>
+    <Modal
+        :modal-open="modalOpen"
+        @update:modalOpen="emit('update:modalOpen', $event)"
+        title="Cadastrar Fornecedor"
+    >
+        <template #body>
+            <form
+                id="form"
+                class="flex flex-col gap-4"
+                @submit.prevent="createSupplier"
+            >
+                <FormSupplier :form="form" />
+            </form>
+        </template>
+        <template #footer>
+            <button
+                type="submit"
+                form="form"
+                class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+                Cadastrar
+            </button>
+        </template>
+    </Modal>
 </template>
